c3fac909731e2d57176d45e939aea76e7a0f53ef
Camila Silva <camilasati@outlook.com>
2018-09-10 23:29:17 -0300

    Implementar login usando useraccounts

---
+++ client/Accounts.js
@@ -0,0 +1,38 @@
+AccountsTemplates.configure({
+	defaultLayoutType: "blaze", //Optional, the default is 'blaze'
+	defaultLayout: "PrincipalLayout",
+	defaultLayoutRegions: {
+		nav: "header",
+	},
+	defaultContentRegion: "main"
+});
+
+AccountsTemplates.configureRoute("signIn");
+
+var pwd = AccountsTemplates.removeField("password");
+AccountsTemplates.removeField("email");
+AccountsTemplates.addFields([
+  {
+    _id: "name",
+    type: "text",
+    displayName: "Full Name",
+    required: true,
+  },
+  {
+    _id: "email",
+    type: "email",
+    required: true,
+    displayName: "email",
+    re: /.+@(.+){2,}\.(.+){2,}/,
+    errStr: "Invalid email",
+  },
+  pwd,
+  {
+    _id: "username",
+    type: "text",
+    displayName: "username",
+    required: true,
+    minLength: 3,
+  },
+]);
+ 
